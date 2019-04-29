52be974e65e860e36cba3e9a5ce0e9c28a3e2db2
Unknown <dannyehb92@gmail.com>
2019-04-28 22:26:43 -0500

    adding a learn and evaluate method, to use in training and testing

--- trainer.py
+++ trainer.py
@@ -5,23 +5,33 @@ import torch.optim as optim
 from model import LSTM
 
 class Trainer(nn.Module):
-    def __init__(self,input_size, hidden_size, batch_size, output_dim, num_layers, learning_rate):
+    def __init__(self,input_size, hidden_size, batch_size, output_dim, num_layers, learning_rate, seq_len):
         super(Trainer, self).__init__()
         self.model = LSTM(input_size, 
                           hidden_size, 
                           batch_size,
                           output_dim, 
-                          num_layers)
+                          num_layers,
+                         seq_len)
         self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
         self.criterion = nn.MSELoss()
         
-    def predict(self, x_train):
-        return self.model(x_train)
-        
-    def learn(self, y_pred, y_train):
+    def learn(self, x_train, y_train):
         self.model.zero_grad()
-#         self.model.hidden = self.model.init_hidden()
-        loss = self.criterion(y_pred, y_train)
-        self.optimiser.zero_grad()
+        y_pred = self.model(x_train)
+        
+        loss = self.criterion(y_pred.t(), y_train)
+        
+        self.optimizer.zero_grad()
         loss.backward()
-        self.optimiser.step()
+        self.optimizer.step()
+        return loss
+    
+    def evaluate(self, x_test, y_test):
+        self.model.eval()
+        with torch.no_grad():
+            y_pred = self.model(x_test)
+            
+        loss = self.criterion(y_pred.t(), y_test)
+        self.model.train()
+        return loss
